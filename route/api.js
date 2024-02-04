de8936691a26761b5977adf232ff9461d8c3d6f0
aptamahendra16 <143199106+aptamahendra16@users.noreply.github.com>
2024-02-04 09:45:10 +0700

    Update api.js

--- route/api.js
+++ route/api.js
@@ -3,7 +3,7 @@ const sh = express.Router();
 
 import { igApi } from 'insta-fetcher';
 
-let ig = new igApi(""); // Cookie instagram.com
+let ig = new igApi("ig_nrcb=1; ds_user_id=8966198065; csrftoken=c4-X8fYp935CViUoyarZ6u; mid=ZanQ0AALAAFYA5--I766YHOfgPch; fbm_124024574287414=base_domain=.instagram.com; ig_did=536D524D-DE87-468C-973A-B6E277CF4229; datr=0NCpZcOGZIPLkzJvQ7nq3oSA; ps_n=0; ps_l=0; dpr=1.25; sessionid=8966198065%3AnFkQQP1cFMppO1%3A24%3AAYfY0ae7Kh4s_F4KJ10J1aPoLVG9Ves5998aYKJ5Ww; rur="EAG\0548966198065\0541738549762:01f7787cc0d661ada9c5103dc3e31866bb24f78cb9a7004a863a2fa54d4e43d7988990c9"; shbid="6077\0548966198065\0541738549766:01f7736485505d90e94f45d8d4fbf733dca483f29b271f5d4e5f3726fde8178ec5fcdb3d"; shbts="1707013766\0548966198065\0541738549766:01f7cd62f4fee985669c34a6f599c4d52e388ef4a31e584c29a9c57e98f37f63388485a2""); // Cookie instagram.com
 
 /* Response messages */
 const creator = 'https://github.com/xznsenpai';
