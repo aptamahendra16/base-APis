6556d46c4ce68ebbfb88c5d26153d16965a8e9a5
aptamahendra16 <143199106+aptamahendra16@users.noreply.github.com>
2024-02-08 21:07:04 +0700

    Update api.js

--- route/api.js
+++ route/api.js
@@ -1,37 +1,36 @@
 const express = require('express');
 const sh = express.Router();
 const { mediafiredl, facebookdl } = require('@bochilteam/scraper');
-const { tiktokdl } = require('tiktokdl')
-const { InstagramService } = require('@xncn/instagramdownloaderpro')
-const { youtube } = require('@xct007/frieren-scraper')
+const { tiktokdl } = require('tiktokdl');
+const { InstagramService } = require('@xncn/instagramdownloaderpro');
+const { youtube } = require('@xct007/frieren-scraper');
+
 const instagram = new InstagramService();
 const creator = 'dnm.my.id';
+
 const noLinkMessage = {
   creator: creator,
   message: 'Sorry, no URL was entered in.'
 };
-const noUserMessage = {
-  creator: creator,
-  message: 'Sorry, no USER was entered in.'
-};
+
 const errorMessage = {
   creator: creator,
   message: 'Sorry, there was an internal error on the server. Please try again later or contact the technical support team.'
 };
 
 sh.get('/instagram', async (req, res) => {
-    if (!req.query.url) {
-        const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
-        return res.status(400).send(stringifiedNoLinkMessage);
-      }
+  if (!req.query.url) {
+    const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
+    return res.status(400).send(stringifiedNoLinkMessage);
+  }
 
   instagram.downloadService.Download(req.query.url)
     .then((result) => {
-        const stringifiedResult = JSON.stringify({
-            creator: creator,
-            ...result,
-            credit: '@xncn'
-        }, (key, value) => (value === undefined ? null : value), 2)
+      const stringifiedResult = JSON.stringify({
+        creator: creator,
+        ...result,
+        credit: '@xncn'
+      }, (key, value) => (value === undefined ? null : value), 2);
 
       res.status(200).send(stringifiedResult);
     })
@@ -44,86 +43,86 @@ sh.get('/instagram', async (req, res) => {
 
 sh.get('/facebookdl', async (req, res) => {
   if (!req.query.url) {
-      const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
-      return res.status(400).send(stringifiedNoLinkMessage);
-    }
+    const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
+    return res.status(400).send(stringifiedNoLinkMessage);
+  }
 
-facebookdl(req.query.url)
-  .then((result) => {
+  facebookdl(req.query.url)
+    .then((result) => {
       const stringifiedResult = JSON.stringify({
-          creator: creator,
-          ...result,
-          credit: '@bochilteam'
-      }, (key, value) => (value === undefined ? null : value), 2)
+        creator: creator,
+        ...result,
+        credit: '@bochilteam'
+      }, (key, value) => (value === undefined ? null : value), 2);
 
-    res.status(200).send(stringifiedResult);
-  })
-  .catch((error) => {
-    console.log(error);
-    const jsonerrorMessage = JSON.stringify(errorMessage, null, 2)
-    res.status(500).send(jsonerrorMessage);
-  });
+      res.status(200).send(stringifiedResult);
+    })
+    .catch((error) => {
+      console.log(error);
+      const jsonerrorMessage = JSON.stringify(errorMessage, null, 2)
+      res.status(500).send(jsonerrorMessage);
+    });
 });
 
 sh.get('/tiktokdl', async (req, res) => {
   if (!req.query.url) {
-      const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
-      return res.status(400).send(stringifiedNoLinkMessage);
-    }
+    const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
+    return res.status(400).send(stringifiedNoLinkMessage);
+  }
 
-tiktokdl(req.query.url)
-  .then((result) => {
+  tiktokdl(req.query.url)
+    .then((result) => {
       const stringifiedResult = JSON.stringify({
-          creator: creator,
-          ...result,
-          credit: 'BOTCAHX'
-      }, (key, value) => (value === undefined ? null : value), 2)
+        creator: creator,
+        ...result,
+        credit: 'BOTCAHX'
+      }, (key, value) => (value === undefined ? null : value), 2);
 
-    res.status(200).send(stringifiedResult);
-  })
-  .catch((error) => {
-    console.log(error);
-    const jsonerrorMessage = JSON.stringify(errorMessage, null, 2)
-    res.status(500).send(jsonerrorMessage);
-  });
+      res.status(200).send(stringifiedResult);
+    })
+    .catch((error) => {
+      console.log(error);
+      const jsonerrorMessage = JSON.stringify(errorMessage, null, 2)
+      res.status(500).send(jsonerrorMessage);
+    });
 });
 
 sh.get('/youtube', async (req, res) => {
   if (!req.query.url) {
-      const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
-      return res.status(400).send(stringifiedNoLinkMessage);
-    }
+    const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
+    return res.status(400).send(stringifiedNoLinkMessage);
+  }
 
   youtube.download(req.query.url)
     .then((result) => {
       const stringifiedResult = JSON.stringify({
-          creator: creator,
-          ...result,
-          credit: '@xct007'
-      }, (key, value) => (value === undefined ? null : value), 2)
+        creator: creator,
+        ...result,
+        credit: '@xct007'
+      }, (key, value) => (value === undefined ? null : value), 2);
 
-    res.status(200).send(stringifiedResult);
-  })
-  .catch((error) => {
-    console.log(error);
-    const jsonerrorMessage = JSON.stringify(errorMessage, null, 2)
-    res.status(500).send(jsonerrorMessage);
-  });
+      res.status(200).send(stringifiedResult);
+    })
+    .catch((error) => {
+      console.log(error);
+      const jsonerrorMessage = JSON.stringify(errorMessage, null, 2)
+      res.status(500).send(jsonerrorMessage);
+    });
 });
 
 sh.get('/mediafiredl', async (req, res) => {
-    if (!req.query.url) {
-        const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
-        return res.status(400).send(stringifiedNoLinkMessage);
-      }
+  if (!req.query.url) {
+    const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
+    return res.status(400).send(stringifiedNoLinkMessage);
+  }
 
   mediafiredl(req.query.url)
     .then((result) => {
-        const stringifiedResult = JSON.stringify({
-            creator: creator,
-            ...result,
-            credit: '@bochilteam'
-        }, (key, value) => (value === undefined ? null : value), 2)
+      const stringifiedResult = JSON.stringify({
+        creator: creator,
+        ...result,
+        credit: '@bochilteam'
+      }, (key, value) => (value === undefined ? null : value), 2);
 
       res.status(200).send(stringifiedResult);
     })
@@ -135,6 +134,3 @@ sh.get('/mediafiredl', async (req, res) => {
 });
 
 module.exports = sh;
-/*
-Author: https://github.com/xznsenpai
-*/
