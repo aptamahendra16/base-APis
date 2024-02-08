1492674ec442f3b25e2791ae9f0031e88b31596a
aptamahendra16 <143199106+aptamahendra16@users.noreply.github.com>
2024-02-09 06:19:26 +0700

    Update random.js

--- route/random.js
+++ route/random.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const sh = express.Router();
 const { simtalk } = require('simsimi-api');
+const { pinterest } = require('@bochilteam/scraper');
 
 const creator = 'dnm.my.id';
 
 const noLinkMessage = {
   creator: creator,
-  message: 'Sorry, no URL was entered in.'
+  message: 'Sorry, no URL / text was entered in.'
 };
 
 const errorMessage = {
@@ -26,6 +27,29 @@ sh.get('/simtalk', async (req, res) => {
         creator: creator,
         ...result,
         credit: 'Leanhtruonggggg'
+      }, (key, value) => (value === undefined ? null : value), 2);
+
+      res.status(200).send(stringifiedResult);
+    })
+    .catch((error) => {
+      console.log(error);
+      const jsonerrorMessage = JSON.stringify(errorMessage, null, 2)
+      res.status(500).send(jsonerrorMessage);
+    });
+});
+
+sh.get('/pinterest', async (req, res) => {
+  if (!req.query.text) {
+    const stringifiedNoLinkMessage = JSON.stringify(noLinkMessage, null, 2);
+    return res.status(400).send(stringifiedNoLinkMessage);
+  }
+
+  pinterest(req.query.text)
+    .then((result) => {
+      const stringifiedResult = JSON.stringify({
+        creator: creator,
+        ...result,
+        credit: '@bochilteam'
       }, (key, value) => (value === undefined ? null : value), 2);
 
       res.status(200).send(stringifiedResult);
