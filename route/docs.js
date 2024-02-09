41657be6db958d8183548e18d8f7ae9a97d2fa41
aptamahendra16 <143199106+aptamahendra16@users.noreply.github.com>
2024-02-09 19:11:56 +0700

    Update docs.js

--- route/docs.js
+++ route/docs.js
@@ -207,6 +207,26 @@ const options = {
  *         description: Data not found or invalid endpoint.
  */
 
+/**
+ * @swagger
+ * /random/chord:
+ *   get:
+ *     tags: [Random]
+ *     parameters:
+ *       - in: query
+ *         name: text
+ *         schema:
+ *           type: string
+ *           format: text
+ *         required: true
+ *         description: Input text for chord
+ *     responses:
+ *       200:
+ *         description: Request executed successfully.
+ *       404:
+ *         description: Data not found or invalid endpoint.
+ */
+
 const swaggerDoc = swaggerJSDoc(options);
 sh.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 module.exports = sh;
