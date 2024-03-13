0658581f5259eb9fc108a58e58a54276122c42ce
aptamahendra16 <143199106+aptamahendra16@users.noreply.github.com>
2024-03-13 12:26:43 +0700

    Create chatGpt.js

---
+++ route/api/chatGpt.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+async function chatGpt(query) {
+    try {
+      const response = await axios.post(
+        'https://api.pawan.krd/v1/chat/completions', 
+        {
+          model: "pai-001",
+          max_tokens: 2000,
+          messages: [
+            { role: "system", content: "You are ShiinaBot with model gpt-4-turbo-preview, your owner is dnm!" },
+            { role: "user", content: query }
+          ]
+        },
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `pk-bFaXWYAqcVGngyPigcUusIrmaTLOMmdYtLWeDLCwWgIsEHZO`,
+          },
+        }
+      );
+
+      return response.data
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+}
+
+module.exports = chatGpt;
